f3842b3011bf3529557e0776dc2ac797976905af
Jhonny Marcelo <jhonny-marcelo@outlook.com>
2024-08-31 22:05:01 -0300

    Added server side validation to category

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -27,9 +27,14 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj) 
         {
-            _db.categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index"); 
-        }
+            if (ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+            return View();
+		}
 	}
 }
--- Bulky/BulkyWeb/Models/Category.cs
+++ Bulky/BulkyWeb/Models/Category.cs
@@ -10,8 +10,10 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
+        [Range(1,100, ErrorMessage ="Display Order must be between 1-100")]
         [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
