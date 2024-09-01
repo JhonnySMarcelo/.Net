b265314f10c553a4c5e3f6e590c5ca8ff6f625f8
Jhonny Marcelo <jhonny-marcelo@outlook.com>
2024-08-31 23:49:39 -0300

    Created Edit view and method

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -31,6 +31,7 @@ namespace BulkyWeb.Controllers
             {
                 ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
             }
+
             if (ModelState.IsValid)
             {
                 _db.categories.Add(obj);
@@ -39,6 +40,40 @@ namespace BulkyWeb.Controllers
 			}
 
             return View();
+		}
+
+		public IActionResult Edit(int? id)
+		{
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category category = _db.categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+			return View(category);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
+			}
+
+			if (ModelState.IsValid)
+			{
+				_db.categories.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View();
 		}
 	}
 }
