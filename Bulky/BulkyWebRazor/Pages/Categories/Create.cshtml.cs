44a1593d0c75a0a54fea6ec64ae7f55468d7f88a
Jhonny Marcelo <jhonny-marcelo@outlook.com>
2024-09-03 14:50:53 -0300

    Added create page and method

---
+++ Bulky/BulkyWebRazor/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,30 @@
+using BulkyWebRazor.Data;
+using BulkyWebRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor.Pages.Categories
+{
+	[BindProperties]
+	public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        
+        public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
+        }
+    }
+}
